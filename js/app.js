f0f35fbb614fb50a9e205aaa1d8ff2ada2c0790d
satinder <satindersingh472@gmail.com>
2022-07-19 14:12:43 -0600

    added wed

---
+++ js/app.js
@@ -0,0 +1,35 @@
+let d = new Date();
+console.log(d);
+let months;
+let day = d.getDay();
+console.log(day);
+
+if (day === 2){
+    setInterval(tuesday_box , 2000);
+} else if (day === 3) {
+setInterval(wednesday_box , 3000);
+}
+
+
+
+
+let sunday = document.getElementById(`box_1`);
+let monday = document.getElementById(`box_2`);
+
+let wednesday = document.getElementById(`box_4`);
+let thursday = document.getElementById(`box_5`);
+let friday = document.getElementById(`box_6`);
+let saturday = document.getElementById(`box_7`);
+ 
+function tuesday_box(){
+    let tuesday = document.getElementById(`box_3`);
+    tuesday[`style`][`height`] =`100px`;
+    tuesday[`style`][`width`] =`100px`;
+    tuesday[`style`][`backgroundColor`] = `purple`;
+}
+function wednesday_box(){
+    let tuesday = document.getElementById(`box_3`);
+    tuesday[`style`][`height`] =`100px`;
+    tuesday[`style`][`width`] =`100px`;
+    tuesday[`style`][`backgroundColor`] = `green`;
+}
